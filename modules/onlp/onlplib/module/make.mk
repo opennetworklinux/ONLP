6e930bd7e6f8ca260d3a09c2fce8a7db6b63401c
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-01 16:11:34 -0700

    Remove ucli.

--- modules/onlp/onlplib/module/make.mk
+++ modules/onlp/onlplib/module/make.mk
@@ -1,10 +1,10 @@
 ###############################################################################
 #
-# 
+#
 #
 ###############################################################################
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 onlplib_INCLUDES := -I $(THIS_DIR)inc
 onlplib_INTERNAL_INCLUDES := -I $(THIS_DIR)src
-onlplib_DEPENDMODULE_ENTRIES := init:onlplib ucli:onlplib
+onlplib_DEPENDMODULE_ENTRIES := init:onlplib
 
