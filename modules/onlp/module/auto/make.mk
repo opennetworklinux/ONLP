747fd7b58317f8665a08fe4a9ec0e6ae942cf03a
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-01 12:30:44 -0700

    Rearranged.

--- modules/onlp/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# onlp Autogeneration
-#
-###############################################################################
-onlp_AUTO_DEFS := module/auto/onlp.yml
-onlp_AUTO_DIRS := module/inc/onlp module/src
-include $(BUILDER)/auto.mk
-
--- modules/onlp/utest/_make.mk
+++
@@ -1,8 +0,0 @@
-###############################################################################
-#
-# onlp Unit Test Makefile.
-#
-###############################################################################
-UMODULE := onlp
-UMODULE_SUBDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(BUILDER)/utest.mk
