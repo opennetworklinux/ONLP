d26091047cec062eb4a5d694b73656077825dbe2
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-03 13:16:13 -0700

    Initial

---
+++ modules/onlp/onlp/module/src/onlp_main.c
@@ -0,0 +1,18 @@
+/************************************************************
+ * <bsn.cl fy=2014 v=onl>
+ * </bsn.cl>
+ ************************************************************
+ *
+ *
+ *
+ ***********************************************************/
+#include <onlp/onlp.h>
+
+int
+onlpdump_main(int argc, char* argv[])
+{
+    onlp_init();
+    onlp_platform_dump(&aim_pvs_stdout,
+                       ONLP_OID_SHOW_F_RECURSE | ONLP_OID_SHOW_F_EVEN_IF_ABSENT);
+    return 0;
+}
