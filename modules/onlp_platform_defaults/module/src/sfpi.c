a09b0f8e0a72ed2cea5d69f699c359dc2071d05b
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-06-03 15:38:06 -0700

    The SFP post-insert handler now takes the sff_info_t structure for the SFP.

--- modules/onlp_platform_defaults/module/src/sfpi.c
+++ modules/onlp_platform_defaults/module/src/sfpi.c
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -31,7 +31,7 @@ __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_is_present(int port));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_eeprom_read(int port, uint8_t data[256]));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_enable_set(int port, int enable));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_enable_get(int port, int* enable));
-__ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_post_insert(int port, uint8_t eeprom[256]));
+__ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_post_insert(int port, sff_info_t* sff_info));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_reset(int port));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_status_get(int port, uint32_t* status));
 __ONLP_DEFAULTI_IMPLEMENTATION(onlp_sfpi_denit(void));
--- modules/onlpie/module/src/sfpi.c
+++ modules/onlpie/module/src/sfpi.c
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -127,7 +127,7 @@ onlp_sfpi_enable_get(int port, int* enable)
  * function will be called to perform the (optional) setup.
  */
 int
-onlp_sfpi_post_insert(int port, uint8_t eeprom[256])
+onlp_sfpi_post_insert(int port, sff_info_t* sff_info)
 {
     return ONLP_STATUS_E_UNSUPPORTED;
 }
