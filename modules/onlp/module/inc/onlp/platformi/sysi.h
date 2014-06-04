e652763044a2e6cd50512e588d8e4fdc7e50e825
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-06-03 18:56:41 -0700

    Add onlp_sysi_platform_manage() interface.

--- modules/onlp/module/inc/onlp/platformi/sysi.h
+++ modules/onlp/module/inc/onlp/platformi/sysi.h
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -106,6 +106,15 @@ void onlp_sysi_onie_data_free(uint8_t* data);
  */
 int onlp_sysi_oids_get(onlp_oid_t* table, int max);
 
+
+/**
+ * @brief Perform necessary platform management.
+ * @note This function will be called periodically by the upper-layer
+ * software to give the platform an opportunity to perform any
+ * platform management required for correct operation. This includes
+ * adjusting fan speeds in response to thermal events.
+ */
+void onlp_sysi_platform_manage(void);
 
 /**
  * @brief This function provides a generic ioctl interface.
