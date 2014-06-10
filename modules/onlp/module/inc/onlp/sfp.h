db61cc794d0e0e9e968f102add497b7b1f286892
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-06-09 18:23:38 -0700

    Expose onlp_sfp_post_insert() for applications that perform direct SFP management.

--- modules/onlp/module/inc/onlp/sfp.h
+++ modules/onlp/module/inc/onlp/sfp.h
@@ -1,21 +1,21 @@
 /*************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  *************************************************************
  *
@@ -29,6 +29,7 @@
 #include <onlp/onlp.h>
 #include <AIM/aim_bitmap.h>
 #include <AIM/aim_pvs.h>
+#include <sff/sff.h>
 
 /* <auto.start.enum(onlp_sfp_status).define> */
 /** onlp_sfp_status */
@@ -135,5 +136,10 @@ int onlp_sfp_events_enable_get(int* enable);
  */
 void onlp_sfp_dump(aim_pvs_t* pvs);
 
+/**
+ * @brief Call the SFP post-insertion handler.
+ *
+ */
+int onlp_sfp_post_insert(int port, sff_info_t* info);
 
 #endif /* __ONLP_SFP_H__ */
