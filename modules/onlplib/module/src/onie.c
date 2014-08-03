fc54524de9c154f1de6d48c898c0bc9ffd7779e1
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-08-03 11:38:23 -0700

    Use correct field.

--- modules/onlplib/module/src/onie.c
+++ modules/onlplib/module/src/onie.c
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -314,7 +314,7 @@ onlp_onie_show(onlp_onie_info_t* info, aim_pvs_t* pvs)
     iof_iprintf(&iof, "Serial Number: %s", info->serial_number);
     iof_iprintf(&iof, "MAC: %{mac}", info->mac);
     iof_iprintf(&iof, "MAC Range: %d", info->mac_range);
-    iof_iprintf(&iof, "Manufacturer: %s", info->device_version);
+    iof_iprintf(&iof, "Manufacturer: %s", info->manufacturer);
     iof_iprintf(&iof, "Manufacture Date: %s", info->manufacture_date);
     iof_iprintf(&iof, "Vendor: %s", info->vendor);
     iof_iprintf(&iof, "Platform Name: %s", info->platform_name);
