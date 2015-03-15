691213afa97928e3ddddbc241497a7be3fa6629a
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2015-03-15 16:59:40 -0700

    Add TlvInfo decode option.

--- modules/onlp/module/src/onlp_main.c
+++ modules/onlp/module/src/onlp_main.c
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=onl>
- * 
- *        Copyright 2014, 2015 Big Switch Networks, Inc.       
- * 
+ *
+ *        Copyright 2014, 2015 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -78,8 +78,9 @@ onlpdump_main(int argc, char* argv[])
     int i = 0;
     int p = 0;
     int x = 0;
+    const char* t = NULL;
 
-    while( (c = getopt(argc, argv, "srehdojmipx")) != -1) {
+    while( (c = getopt(argc, argv, "srehdojmipxt:")) != -1) {
         switch(c)
             {
             case 's': show=1; break;
@@ -93,6 +94,7 @@ onlpdump_main(int argc, char* argv[])
             case 'm': m=1; break;
             case 'i': i=1; break;
             case 'p': p=1; show=-1; break;
+            case 't': t = optarg; break;
             default: help=1; rv = 1; break;
             }
     }
@@ -109,7 +111,23 @@ onlpdump_main(int argc, char* argv[])
         printf("  -m   Run platform manager.\n");
         printf("  -i   Iterate OIDs.\n");
         printf("  -p   Show SFP presence.\n");
+        printf("  -t <file>  Decode TlvInfo data.\n");
         return rv;
+    }
+
+    if(t) {
+        int rv;
+        onlp_onie_info_t onie;
+        rv = onlp_onie_decode_file(&onie, t);
+        if(rv >= 0) {
+            onlp_onie_show(&onie, &aim_pvs_stdout);
+            onlp_onie_info_free(&onie);
+            return 0;
+        }
+        else {
+            aim_printf(&aim_pvs_stdout, "Decode failed.");
+            return 1;
+        }
     }
 
     onlp_init();
